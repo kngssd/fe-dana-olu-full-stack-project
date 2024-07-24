bee369b0197316c7f4a9d6d17f509765c3dc76a2
2405-007-dk <danakengessova@gmail.com>
2024-07-24 16:45:56 +0100

    add search box and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { DemonstratingProps } from "./components/demo/DemonstratingProps";
+import React from "react";
+
 // @ts-ignore - supress an error about import.meta
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 function App() {
+    const [searchTerm, setSearchTerm] = React.useState("");
+
+    function handleSearchInput(event) {
+        setSearchTerm(event.target.value);
+        console.log(searchTerm);
+    }
+
+    function handleSearch() {}
     return (
         <div>
             <h1>App component</h1>
             <DemonstratingProps />
-            <h1>{baseURL}</h1>
+            <input
+                placeholder="search"
+                onChange={handleSearchInput}
+                type="text"
+                value={searchTerm}
+            />
+            <button onClick={handleSearch}>Search</button>
         </div>
     );
 }
