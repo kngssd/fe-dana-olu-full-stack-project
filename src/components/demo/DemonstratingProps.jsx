a87fcb34e4a2046dc159af4d1bdbc572d16e1e9d
2405-007-dk <danakengessova@gmail.com>
2024-07-24 10:33:06 +0100

    create repo

--- src/components/demo/DemonstratingProps.jsx
+++ src/components/demo/DemonstratingProps.jsx
@@ -6,7 +6,7 @@ import { Team } from "./Team";
 export function DemonstratingProps() {
     return (
         <div>
-            <h2>Demonstrating props</h2>
+            <h2>Full-stack Project by Olu and Dana</h2>
             <Team title="blue team" scores={[10, 20]} />
             <Team title="red team" scores={[99, -20]} subtitle="hosts" />
         </div>
