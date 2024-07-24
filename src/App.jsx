bdb24b7ce988c9dba1c1caf7b25766d3558acd78
2405-007-dk <danakengessova@gmail.com>
2024-07-24 16:32:03 +0100

    set up linked API  to the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { DemonstratingProps } from "./components/demo/DemonstratingProps";
+// @ts-ignore - supress an error about import.meta
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 function App() {
     return (
         <div>
             <h1>App component</h1>
             <DemonstratingProps />
+            <h1>{baseURL}</h1>
         </div>
     );
 }
