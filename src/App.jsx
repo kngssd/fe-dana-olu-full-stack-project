12e3e7612152c2e74255ac1c91d116049e54dd4e
2405-007-dk <danakengessova@gmail.com>
2024-07-25 14:57:32 +0100

    set up search handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { DemonstratingProps } from "./components/demo/DemonstratingProps";
 import React from "react";
+import axios from "axios";
 
 // @ts-ignore - supress an error about import.meta
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 function App() {
     const [searchTerm, setSearchTerm] = React.useState("");
+    const [movieList, setMovieList] = React.useState([]);
 
     function handleSearchInput(event) {
         setSearchTerm(event.target.value);
         console.log(searchTerm);
     }
 
-    function handleSearch() {}
+    const handleSearch = async () => {
+        try {
+            const httpResponse = await axios.get(baseURL);
+            console.log(httpResponse);
+        } catch (error) {
+            console.error("An error occured", error);
+        }
+    };
     return (
         <div>
             <h1>App component</h1>
