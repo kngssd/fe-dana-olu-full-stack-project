84b66e72ef8d02e65e1307fd885d46520bb7aa98
2405-007-dk <danakengessova@gmail.com>
2024-07-30 15:37:33 +0100

    FSPDO-10 movie card is created

--- src/App.jsx
+++ src/App.jsx
@@ -41,11 +41,9 @@ function App() {
 
     const movieDisplay = movieList.map((movie) => {
         return (
-            <>
-                <div onClick={handleChooseMovie} id={movie.id}>
-                    {movie.name}
-                </div>
-            </>
+            <div key={movie.id} onClick={handleChooseMovie} id={movie.id}>
+                {movie.name}
+            </div>
         );
     });
 
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -4,9 +4,12 @@ export default function MovieCard(props) {
             <h2>{props.movieData.name}</h2>
             <h3>{props.movieData.date}</h3>
             {props.movieData.vote_average && (
-                <h3>{props.movieData.vote_average}/10</h3>
+                <h3>Rating: {props.movieData.vote_average}/10</h3>
             )}
             <h3>{props.movieData.runtime}</h3>
+            {props.movieData.abstract && (
+                <h3>Abstract: {props.movieData.abstract}</h3>
+            )}
         </div>
     );
 }
